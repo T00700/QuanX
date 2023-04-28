72fe9e7e1c2cf4f5eb4ff55a33b72318597fa524
Cube <49463634+RuCu6@users.noreply.github.com>
2023-04-28 12:22:32 +0800

    Update amap.js

--- Scripts/amap.js
+++ Scripts/amap.js
@@ -1,4 +1,4 @@
-// 2023-04-25 09:38
+// 2023-04-28 12:15
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -359,8 +359,15 @@ if (url.includes("/faas/amap-navigation/main-page")) {
       delete obj.data.modules[i];
     });
   }
+} else if (url.includes("/shield/search_poi/city_feed")) {
+  const item = ["city_feed", "transportation"];
+  if (obj.modules) {
+    item.forEach((i) => {
+      delete obj.modules[i];
+    });
+  }
 } else if (url.includes("/shield/search_poi/search/sp")) {
-  if (obj.data.list_data) {
+  if (obj.data?.list_data) {
     let list = obj.data.list_data.content[0];
     // 详情页 底部 房产推广
     if (list?.hookInfo) {
@@ -403,6 +410,17 @@ if (url.includes("/faas/amap-navigation/main-page")) {
       list?.item_type === "ImageBanner"
     ) {
       delete list.card;
+    }
+  } else if (obj.data?.district?.poi_list) {
+    // 搜索列表详情页
+    let poi = obj.data.district.poi_list[0];
+    // 订票横幅
+    if (poi?.transportation) {
+      delete poi.transportation;
+    }
+    // 景点门票 酒店特惠 特色美食 休闲玩乐
+    if (poi?.feed_rec_tab) {
+      delete poi.feed_rec_tab;
     }
   }
 } else if (
