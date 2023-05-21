3abafe2e2b48ef3c4f1fd7c21fab8581e6f2a509
Cube <49463634+RuCu6@users.noreply.github.com>
2023-05-21 17:19:00 +0800

    delete 欧乐

--- Scripts/webpage/olevod.js
+++
@@ -1,5 +0,0 @@
-let rHead = "<head>";
-let newStyle =
-  "<head><style> .ad, .ad1, .ad2, .L4A, .A4R, .wbalist_thumb.ab-light, .L1 .B1L, .B1R{display:none!important} </style>";
-let body = $response.body.replace(rHead, newStyle);
-$done({ body });
