979db3b1f4293074d98b7289b8bb534d1294c905
Cube <49463634+RuCu6@users.noreply.github.com>
2023-07-18 20:02:38 +0800

    update

--- Scripts/break/adrive.js
+++ Scripts/break/adrive.js
@@ -1,4 +1,4 @@
-// 2023-07-18 18:58
+// 2023-07-18 19:05
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -6,7 +6,7 @@ let obj = JSON.parse($response.body);
 
 if (url.includes("/v1/users/home/widgets")) {
   const item = [
-    "recentUsed", // 最近在看
+    // "recentUsed", // 最近在看
     "coreFeatures", // 顶部图标
     "activities", // 精选活动
     "myBackup", // 我的备份
