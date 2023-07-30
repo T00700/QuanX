26e0393ee2fcc1facacd8a52549c5f7285a6ea37
Cube <49463634+RuCu6@users.noreply.github.com>
2023-07-30 18:50:34 +0800

    JavDB

--- Scripts/break/javdb.js
+++ Scripts/break/javdb.js
@@ -1,9 +1,9 @@
-// 2023-07-28 16:15
+// 2023-07-30 18:35
 
 const url = $request.url;
 let header = $request.headers;
 
-if (url.includes("/play")) {
+if (url.includes("/api/v1/movies/") && url.includes("/play?")) {
   header.authorization =
     "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6OTc1NDMwLCJ1c2VybmFtZSI6IndlaWd1YW5naHQifQ.lyfGvtZcz0SjiKNx-k9Aoe_UgcMyxwG4Xqq3lzvbIao";
   $done({ headers: header });
--- Scripts/myBlockAds.js
+++ Scripts/myBlockAds.js
@@ -1,4 +1,4 @@
-// 2023-07-30 17:10
+// 2023-07-30 18:45
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -45,21 +45,42 @@ if (body) {
       }
       break;
     // JavDB
-    case /^https:\/\/(api\.yijingluowangluo\.xyz|jdforrepam\.com)\/api\/v1\/(ads|startup|users)/.test(
+    case /^https:\/\/(api\.yijingluowangluo\.xyz|jdforrepam\.com)\/api\/v\d\/\w+/.test(
       url
     ):
       try {
         let obj = JSON.parse(body);
-        if (obj.data?.splash_ad) {
-          obj.data.splash_ad.enabled = false;
-          obj.data.splash_ad.overtime = 0;
-        }
-        if (obj.data?.ads) {
-          obj.data.ads = {};
-        }
-        if (obj.data?.user) {
-          obj.data.user.vip_expired_at = "2101-06-08T17:35:01.000+08:00";
-          obj.data.user.is_vip = true;
+        if (url.includes("/api/v1/ads")) {
+          if (obj.data?.enabled) {
+            obj.data.enabled = false;
+          }
+          if (obj.data?.ads) {
+            obj.data.ads = {};
+          }
+        } else if (url.includes("/api/v1/startup")) {
+          if (obj.data?.splash_ad) {
+            obj.data.splash_ad.enabled = false;
+            obj.data.splash_ad.overtime = 0;
+          }
+          if (obj.data?.feedback) {
+            obj.data.feedback = {};
+          }
+          if (obj.data?.settings?.NOTICE) {
+            delete obj.data.settings.NOTICE;
+          }
+          if (obj.data?.user) {
+            obj.data.user.vip_expired_at = "2101-06-08T17:35:01.000+08:00";
+            obj.data.user.is_vip = true;
+          }
+        } else if (url.includes("/api/v1/users")) {
+          if (obj.data?.user) {
+            obj.data.user.vip_expired_at = "2101-06-08T17:35:01.000+08:00";
+            obj.data.user.is_vip = true;
+          }
+        } else if (url.includes("/api/v4/movies/")) {
+          if (obj.data?.show_vip_banner) {
+            obj.data.show_vip_banner = false;
+          }
         }
         body = JSON.stringify(obj);
       } catch (error) {
