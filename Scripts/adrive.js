6faed89b86961ebdcbfb67c6d203af10207f35f4
Cube <49463634+RuCu6@users.noreply.github.com>
2023-08-02 09:36:37 +0800

    Add files via upload

---
+++ Scripts/adrive.js
@@ -0,0 +1,26 @@
+// 2023-08-02 09:25
+
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (url.includes("/v2/users/home/news")) {
+  if (obj.result.length) {
+    obj.result = obj.result.filter(
+      (i) => !i?.code?.includes("productUpdate")
+    );
+  }
+} else if (url.includes("/v2/users/home/widgets")) {
+  const item = [
+    "banners", // 顶部奖励
+    "coreFeatures", // 横版图标
+    "mainBackup", // 手机备份
+    "minorBackup", // 备份设备列表
+    "signIn" // 签到
+  ];
+  item.forEach((i) => {
+    delete obj[i];
+  });
+}
+
+$done({ body: JSON.stringify(obj) });
