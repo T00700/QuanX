b9e4c72b86e54d039a172cdedc4d802f023a1a17
Cube <49463634+RuCu6@users.noreply.github.com>
2023-08-14 08:50:55 +0800

    Update didi.js

--- Scripts/didi.js
+++ Scripts/didi.js
@@ -1,5 +1,15 @@
 // 2023-08-08 08:55
 
+/**
+ * [rewrite_local]
+ * # 滴滴车主
+ * ^https:\/\/guard\.sec\.xiaojukeji\.com\/api\/driverGuard\/getShieldStatus\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/didi.js
+ * ^https:\/\/gw\.am\.xiaojukeji\.com\/driverlife\/client\/driver\/homepage\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/didi.js
+ * 
+ * [mitm]
+ * hostname = g*.xiaojukeji.com
+ */
+
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
