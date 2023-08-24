8d6d72e0fbd7b8e86d70daac4978a928e488cea6
Cube <49463634+RuCu6@users.noreply.github.com>
2023-08-24 13:04:31 +0800

    添加 阿里巴巴

--- Scripts/amdc.js
+++ Scripts/amdc.js
@@ -1,4 +1,4 @@
-// 2023-08-22 15:45
+// 2023-08-24 12:45
 
 const url = $request.url;
 const header = $request.headers;
@@ -8,6 +8,7 @@ let ua = header["User-Agent"] || header["user-agent"];
 if (url.includes("/amdc/mobileDispatch")) {
   if (
     ua.includes("AMapiPhone") || // 高德地图
+    ua.includes("Alibaba") || // 阿里巴巴
     ua.includes("Cainiao4iPhone") || // 菜鸟
     ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") // 飞猪旅行
   ) {
