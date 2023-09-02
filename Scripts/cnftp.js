f1e0c6ab370bea95778cc51ea5081d881635b2c2
Cube <49463634+RuCu6@users.noreply.github.com>
2023-09-02 17:29:16 +0800

    Add files via upload

--- Scripts/cnftp.js
+++ Scripts/cnftp.js
@@ -1,4 +1,4 @@
-// 2023-09-02 14:10
+// 2023-09-02 17:25
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -35,9 +35,7 @@ if (isIQY) {
         "vr"
       ];
       for (let i of item) {
-        if (obj.content.resource?.[i]) {
-          delete obj.content.resource[i];
-        }
+        delete obj.content.resource[i];
       }
     }
   } else if (url.includes("/control/")) {
@@ -48,11 +46,8 @@ if (isIQY) {
   } else if (url.includes("/getMyMenus?")) {
     // 我的页面
     if (obj?.data?.length > 0) {
-      obj.data = obj.data.filter(
-        (i) =>
-          !["wd_liebiao_2", "wd_liebiao_3", "wd_liebiao_4"]?.includes(
-            i?.statistic?.block
-          )
+      obj.data = obj.data.filter((i) =>
+        !["wd_liebiao_2", "wd_liebiao_3", "wd_liebiao_4"]?.includes(i?.statistic?.block)
       );
     }
   } else if (url.includes("/home_top_menu?")) {
@@ -61,8 +56,8 @@ if (isIQY) {
       let card = obj.cards[0];
       if (card?.items?.length > 0) {
         // 1017直播 8196热点 4525518866820370中国梦
-        card.items = card.items.filter(
-          (i) => !["1017", "8196", "4525518866820370"]?.includes(i?._id)
+        card.items = card.items.filter((i) =>
+          !["1017", "8196", "4525518866820370"]?.includes(i?._id)
         );
         for (let i = 0; i < card.items.length; i++) {
           card.items[i].show_order = i + 1;
@@ -80,9 +75,7 @@ if (isIQY) {
     if (obj) {
       const item = ["adSlots", "splashLottieFile", "splashUiConfig"];
       for (let i of item) {
-        if (obj?.[i]) {
-          delete obj[i];
-        }
+        delete obj[i];
       }
     }
   } else if (url.includes("/search.video.iqiyi.com/")) {
@@ -99,36 +92,35 @@ if (isIQY) {
   } else if (url.includes("/views_category/")) {
     // 电视剧版块
     if (obj?.cards?.length > 0) {
-      obj.cards = obj.cards.filter(
-        (i) =>
-          i.hasOwnProperty("alias_name") &&
-          !["ad_mobile_flow", "ad_trueview", "tv-jiaodiantu"]?.includes(
-            i?.alias_name
-          )
+      obj.cards = obj.cards.filter((i) =>
+        ![
+          "ad_mobile_flow",
+          "ad_trueview",
+          "tv-jiaodiantu"
+        ]?.includes(i?.alias_name)
       );
     }
   } else if (url.includes("/views_comment/")) {
     // 播放页评论区
     if (obj?.cards?.length > 0) {
       // 评论资源位
-      obj.cards = obj.cards.filter(
-        (i) =>
-          i.hasOwnProperty("alias_name") &&
-          !i?.alias_name?.includes("comment_resource_card") &&
-          !i?.alias_name?.includes("comment_resource_convention_card")
+      obj.cards = obj.cards.filter((i) =>
+        ![
+          "comment_resource_card",
+          "comment_resource_convention_card"
+        ]?.includes(i?.alias_name)
       );
     }
   } else if (url.includes("/views_home/")) {
     // 首页信息流样式1
     if (obj?.cards?.length > 0) {
-      obj.cards = obj.cards.filter(
-        (i) =>
-          ![
-            "ad_mobile_flow", // 信息流广告
-            "ad_trueview", //信息流广告
-            "focus", // 顶部横版广告
-            "qy_home_vip_opr_banner" // 会员营销banner
-          ]?.includes(i?.alias_name)
+      obj.cards = obj.cards.filter((i) =>
+        ![
+          "ad_mobile_flow", // 信息流广告
+          "ad_trueview", //信息流广告
+          "focus", // 顶部横版广告
+          "qy_home_vip_opr_banner" // 会员营销banner
+        ]?.includes(i?.alias_name)
       );
     }
   } else if (url.includes("/views_plt/")) {
@@ -137,47 +129,45 @@ if (isIQY) {
       // 云影院卡片 vip优惠购买卡片
       const item = ["cloud_cinema", "vip_fixed_card"];
       for (let i of item) {
-        if (obj?.kv_pair?.[i]) {
-          delete obj.kv_pair[i];
-        }
+        delete obj.kv_pair[i];
       }
     }
     if (obj?.cards?.length > 0) {
-      obj.cards = obj.cards.filter(
-        (i) =>
-          ![
-            // "cloud_cinema_detail_character", // 云影院演员列表
-            // "cloud_cinema_detail_synopsis", // 云影院详情简介
-            // "cloud_cinema_play_detail_tag", // 云影院详情标签
-            "cloud_cinema_play_privilege", // 云影院底部文字
-            "cloud_cinema_playlist", // 云影院播单
-            "cloud_cinema_playlist_1", // 云影院播单2
-            "cloud_cinema_playlist_2", // 云影院播单3
-            // "cloud_cinema_preview_collection", // 云影院预告片选集
-            "cloud_cinema_privilege_icon", // 云影院内容权益
-            "cloud_cinema_star_activities", // 云影院推广横幅
-            "play_ad_no_vip", // 视频关联广告
-            "play_around", // 周边视频 短视频
-            // "play_collection", // 选集
-            "play_custom_card", // 偶像练习生定制卡片
-            // "play_detail_tag", // 详情标签
-            // "play_rap_custom", // 综艺 svip舞台纯享
-            // "play_series_collection", // 综艺 选集
-            "play_splendid_collection", // 综艺 合集 正片没有的都在这里
-            // "play_type_topical_card_3", // 综艺 幕后花絮
-            "play_type_topical_card_4", // 综艺 精彩二创
-            "play_variety_custom_2", // 综艺 精彩看点
-            "play_vertical", // 综艺 竖屏内容
-            "play_vip_promotion", // 会员推广
-            "play_water_fall_like", // 猜你喜欢
-            "play_water_fall_like_title", // 猜你喜欢标题
-            "plt_cloud_cinema_photo", // 云影院剧照 清晰度低
-            // "plt_cloud_cinema_short1", // 云影院官方短视频
-            "plt_cloud_cinema_short2", // 云影院短视频剪辑
-            // "plt_playlist", // 播单
-            "plt_playlist_1", // 播单2
-            "funny_short_video" // 精彩短视频
-          ]?.includes(i?.alias_name)
+      obj.cards = obj.cards.filter((i) =>
+        ![
+          // "cloud_cinema_detail_character", // 云影院演员列表
+          // "cloud_cinema_detail_synopsis", // 云影院详情简介
+          // "cloud_cinema_play_detail_tag", // 云影院详情标签
+          "cloud_cinema_play_privilege", // 云影院底部文字
+          "cloud_cinema_playlist", // 云影院播单
+          "cloud_cinema_playlist_1", // 云影院播单2
+          "cloud_cinema_playlist_2", // 云影院播单3
+          // "cloud_cinema_preview_collection", // 云影院预告片选集
+          "cloud_cinema_privilege_icon", // 云影院内容权益
+          "cloud_cinema_star_activities", // 云影院推广横幅
+          "play_ad_no_vip", // 视频关联广告
+          "play_around", // 周边视频 短视频
+          // "play_collection", // 选集
+          "play_custom_card", // 偶像练习生定制卡片
+          // "play_detail_tag", // 详情标签
+          // "play_rap_custom", // 综艺 svip舞台纯享
+          // "play_series_collection", // 综艺 选集 看点
+          "play_splendid_collection", // 综艺 合集 正片没有的都在这里
+          "play_type_topical_card_3", // 综艺 幕后花絮
+          "play_type_topical_card_4", // 综艺 精彩二创
+          "play_variety_custom_2", // 综艺 精彩看点
+          "play_vertical", // 综艺 竖屏内容
+          "play_vip_promotion", // 会员推广
+          "play_water_fall_like", // 猜你喜欢
+          "play_water_fall_like_title", // 猜你喜欢标题
+          "plt_cloud_cinema_photo", // 云影院剧照 清晰度低
+          // "plt_cloud_cinema_short1", // 云影院官方短视频
+          "plt_cloud_cinema_short2", // 云影院短视频剪辑
+          "plt_playlist", // 播单
+          "plt_playlist_1", // 播单2
+          "plt_playlist_2", // 播单3
+          "funny_short_video" // 精彩短视频
+        ]?.includes(i?.alias_name)
       );
     }
   } else if (url.includes("/views_search/")) {
@@ -382,9 +372,7 @@ if (isIQY) {
     if (obj?.data) {
       const item = ["tipContent", "tipContentNew"];
       for (let i of item) {
-        if (obj?.data?.[i]) {
-          delete obj.data[i];
-        }
+        delete obj.data[i];
       }
     }
   } else if (url.includes("columbus.gateway.new.execute")) {
@@ -403,29 +391,19 @@ if (isIQY) {
       if (objData?.nodes?.length > 0) {
         let node0 = objData.nodes[0];
         if (node0?.nodes?.length > 0) {
-          // 16120球区自动化组件
-          // 16121播放页选集组件
-          // 16130播放页广告组件
-          // 61115播放页活动组件
-          // 87463播放页有料不能停组件
-          // 87582播放页全屏播后推荐组件
-          // 198314播放页会员引导组件
-          // 238789播放页用户触达组件
-          // 240290播放页推荐组件
-          // 257980播放页简介组件
-          // 257982播放页功能bar组件
-          node0.nodes = node0.nodes.filter(
-            (i) =>
-              ![
-                "Phone运营banner",
-                "播放页广告组件",
-                "播放页活动组件",
-                "播放页全屏播后推荐组件",
-                "播放页会员引导组件",
-                "播放页用户触达组件",
-                "球区自动化组件",
-                "优酷购"
-              ]?.includes(i?.typeName)
+          node0.nodes = node0.nodes.filter((i) =>
+            ![
+              "Phone运营banner",
+              "播放页广告组件",
+              "播放页会员引导组件",
+              "播放页活动组件",
+              "播放页全屏播后推荐组件",
+              "播放页推荐组件",
+              "播放页用户触达组件",
+              "播放页有料不能停组件",
+              "球区自动化组件",
+              "优酷购"
+            ]?.includes(i?.typeName)
           );
         }
       }
@@ -560,9 +538,7 @@ if (isIQY) {
     if (obj?.data?.data) {
       const item = ["ad", "watermark", "ykad"];
       for (let i of item) {
-        if (obj?.data?.data?.[i]) {
-          delete obj.data.data[i];
-        }
+        delete obj.data.data[i];
       }
     }
   }
